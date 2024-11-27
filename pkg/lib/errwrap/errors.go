97439b892b0356d285fe68b471158ef7418d90f6
nsaltun <enal25@hotmail.com>
2024-11-27 16:32:29 +0300

    lib change err file names

--- pkg/lib/errwrap/errors.go
+++ pkg/lib/errwrap/errors.go
@@ -1,10 +1,69 @@
 package errwrap
 
-import "net/http"
+import "fmt"
 
-var (
-	ErrBadRequest = NewError("invalid argument", "400").SetHttpCode(http.StatusBadRequest)
-	ErrNotFound   = NewError("resource not found", "404").SetHttpCode(http.StatusNotFound)
-	ErrConflict   = NewError("already exists", "409").SetHttpCode(http.StatusConflict)
-	ErrInternal   = NewError("internal server error", "500").SetHttpCode(http.StatusInternalServerError)
-)
+type IError interface {
+	error
+	SetMessage(msg string) IError
+	SetHttpCode(code int) IError
+	HttpCode() int
+	ErrorResp() ErrorResponse
+}
+
+type errorWrapper struct {
+	message   string
+	code      string
+	httpCode  int
+	originErr error
+}
+
+type ErrorResponse struct {
+	Message string
+	Code    string
+}
+
+func NewError(msg string, code string) IError {
+	return &errorWrapper{
+		message: msg,
+		code:    code,
+	}
+}
+
+func (e *errorWrapper) SetMessage(msg string) IError {
+	newErr := e.clone()
+	newErr.message = msg
+	return newErr
+}
+
+func (e *errorWrapper) SetHttpCode(code int) IError {
+	newErr := e.clone()
+	newErr.httpCode = code
+	return newErr
+}
+
+func (e *errorWrapper) HttpCode() int {
+	return e.httpCode
+}
+
+func (e *errorWrapper) ErrorResp() ErrorResponse {
+	return ErrorResponse{
+		Code:    e.code,
+		Message: e.message,
+	}
+}
+
+func (e *errorWrapper) Error() string {
+	return fmt.Sprintf("%s code:%s", e.message, e.code)
+}
+
+func (e *errorWrapper) clone() *errorWrapper {
+	if e == nil {
+		return nil
+	}
+	return &errorWrapper{
+		code:      e.code,
+		httpCode:  e.httpCode,
+		message:   e.message,
+		originErr: e.originErr,
+	}
+}
