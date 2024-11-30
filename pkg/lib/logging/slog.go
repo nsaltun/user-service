f2382738fa10bd386554a51de0b55bd9e2a7e3dc
nsaltun <enal25@hotmail.com>
2024-11-30 13:37:52 +0300

    loglevel configurable

--- pkg/lib/logging/slog.go
+++ pkg/lib/logging/slog.go
@@ -4,12 +4,26 @@ import (
 	"log/slog"
 	"os"
 	"time"
+
+	"github.com/spf13/viper"
 )
 
+var levelMap = map[string]slog.Leveler{
+	"DEBUG": slog.LevelDebug,
+	"INFO":  slog.LevelInfo,
+	"WARN":  slog.LevelWarn,
+	"ERROR": slog.LevelError}
+
 func InitSlog() {
+	vi := viper.New()
+	vi.AutomaticEnv()
+	level := "INFO"
+	vi.SetDefault("LOG_LEVEL", level)
+	level = vi.GetString("LOG_LEVEL")
+
 	// Set up a JSON handler for logging
 	jsonHandler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level: slog.LevelInfo,
+		Level: levelMap[level],
 		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
 			// Replace the default time attribute with a UTC time
 			if a.Key == slog.TimeKey {
